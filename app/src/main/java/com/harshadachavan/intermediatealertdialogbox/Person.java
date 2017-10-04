db9a7a101ab14c05c8556a1cad079bb986a529b7
Harshada <harshada@destaglobal.com>
2017-10-04 18:30:00 +0530

    Alert Dialog box in menu

---
+++ app/src/main/java/com/harshadachavan/intermediatealertdialogbox/Person.java
@@ -0,0 +1,12 @@
+package com.harshadachavan.intermediatealertdialogbox;
+
+/**
+ * Created by Harshada Chavan on 10/4/2017.
+ */
+
+public class Person {
+
+    public String nameOfPerson;
+    public String phoneNumber;
+    public String dateBirth;
+}
